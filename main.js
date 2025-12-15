d76d85f79dc092d5026465439fde79fecec58542
Wallrisss <Robbyrev7@gmail.com>
2025-12-15 14:24:57 +0000

    dash done and boss atack 1 started

--- main.js
+++ main.js
@@ -20,19 +20,23 @@ let gi = new GameInterface();
 /* Variables: Top-Level variables defined here are used to hold game state */
 //bullets x and y
 //bullet amount
-//boss x and y
-//ax
+
 //ax speed
 //damage
 //hp amount
 let hearts = 0
 //player movement shenanigans
 let px = 100
-let py = 100
+let py = 300
 let ps = 12
 let dashCD = 99
+//boss x and y
+let bx=750;
+let by=300;
+//ax
+let axx = bx+100;
+let axy = by+100;
 /* Drawing Functions */
-
 /* Example drawing function: you can add multiple drawing functions
 that will be called in sequence each frame. It's a good idea to do 
 one function per each object you are putting on screen, and you
@@ -53,7 +57,11 @@ gi.addDrawing(
 /* Example: Mouse click handler (you can change to handle 
 any type of event -- keydown, mousemove, etc) */
 
-
+let enemyAttack = {
+  1: false,
+  2: false,
+  3: false,
+}
 /* Mr. Hinkle showed how to use a keysDown object to track
 which keys are currently down with separate keydown and keyup
 handlers and then an addDrawing for smooth updates :)
@@ -129,20 +137,68 @@ gi.addDrawing(
     }   
   }
 )
+//dash cooldown
 gi.addDrawing(
   function ({stepTime}) {
      if (dashCD < 99){
       
-      dashCD += stepTime;}
+      dashCD += stepTime/20;}
       if (dashCD > 99){
        dashCD = 99;}
+//speed reset
 if (ps > 12){
-  ps -= stepTime;
+  ps -= stepTime/10;
+if (ps < 12){
+  ps = 12;
+};
+//boundarys
 }
-if (px >= 1000){
- px == 10
+if (px >= 1450){
+  px = 1450;
+} 
+if (px <= 0){
+ px = 0;
 }
-  }
+if (py >= 750){
+ py = 750;
+}
+if (py <= 0){
+ py = 0;
+}}
+)
+//Boss
+gi.addDrawing(
+  function ({ ctx, width, height, elapsed, stepTime }) {
+    // Your drawing code here...  
+    ctx.beginPath(); 
+    ctx.fillStyle = "red";
+    ctx.arc(bx,by,50,0,Math.PI*2);
+    ctx.fill(); 
+  })
+//Ax
+gi.addDrawing(
+  function ({ ctx, width, height, elapsed, stepTime }) {
+    // Your drawing code here...  
+    ctx.beginPath(); 
+    ctx.strokeStyle = "orange";
+    ctx.moveTo(bx,by);
+    ctx.lineTo(axx,axy);
+    ctx.stroke(); 
+  })
+  //attack 1
+  //pulls ax back until click and then boss dashes while spinning ax
+  gi.addDrawing(
+    function ({ stepTime }) {
+      if (enemyAttack[1]) {
+        // move boss toward player on x axis
+        if (px > bx) { bx += stepTime/2; }
+        else if (px < bx) { bx -= stepTime/2; }
+        // move boss toward player on y axis
+        if (py > by) { by += stepTime/2; }
+        else if (py < by) { by -= stepTime/2; }
+      }
+    }
+  )
 /* Run the game */
 gi.run();
 
